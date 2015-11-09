259454e542ce15807b37ffa47be1ee1a2773e4be
Eugene Glyantsev <glyantsev_e@mail.ru>
2015-11-10 02:50:56 +0300

    new recursive model for tree

---
+++ trader/web/src/main/java/net/bir/web/beans/adaptors/FileSystemBean.java
@@ -0,0 +1,21 @@
+package net.bir.web.beans.adaptors;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class FileSystemBean {
+    private static final String SRC_PATH = "/WEB-INF";
+    private List<FileSystemNode> srcRoots;
+
+    public synchronized List<FileSystemNode> getSourceRoots() {
+        if (srcRoots == null) {
+            srcRoots = new FileSystemNode(SRC_PATH).getDirectories();
+        }
+
+        return srcRoots;
+    }
+}
