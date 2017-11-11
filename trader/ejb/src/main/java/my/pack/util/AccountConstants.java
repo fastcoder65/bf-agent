e7279bec264785886eed8504f644b11e91a97761
Eugene Glyantsev <glyantsev_e@mail.ru>
2017-11-11 18:08:31 +0300

    new recursive model for tree

--- trader/ejb/src/main/java/my/pack/util/AccountConstants.java
+++ trader/ejb/src/main/java/my/pack/util/AccountConstants.java
@@ -7,15 +7,7 @@ package my.pack.util;
  */
 
 public class AccountConstants {
-	
 	public static final String APP_KEY = "SGwftqga8eArLpAT";
-//	public static final String APP_KEY_DELAYED = "90EhQuo32PR3cKeJ";
-//	public static final String USERNAME = "fastcoder65";
-//	public static final String PASSWORD = "cjyxeltcysq123-456";
-	
 	public static final String ENDPOINT_TO_CERTLOGIN = "https://identitysso.betfair.com/api/certlogin";
-//	public static final String PATH_TO_PRIVATE_KEY = "C:\\Users\\Eugene\\Documents\\amazon\\cert\\dev_fastcoder65.p12";
-
 	public static final String PATH_TO_PRIVATE_KEY = "/opt/certstore/github_ks.p12";
-
 }
